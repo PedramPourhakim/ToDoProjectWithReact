cc8ad057e740d1b0f6b38d28d4a3e7900fae185a
Pedram <shemuel1226@gmail.com>
2023-03-21 20:43:15 +0330

    ProjectDone!

--- src/TaskItems.jsx
+++ src/TaskItems.jsx
@@ -1,28 +1,63 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { taskContext } from './taskContext';
 
 const TaskItems = ()=>{
-    return (
-        <ul className="list-group m-0 p-0 mt-2">
-            <li className="list-group-item d-flex justify-content-between">
-                کار شماره 1
-                <span>
-                    <i className="me-3 pointer fas fa-check text-success transition_200 text_hover_shadow"></i>
-                    <i className="me-3 pointer fas fa-times text-warning transition_200 text_hover_shadow"></i>
-                    <i className="me-3 pointer fas fa-trash text-danger transition_200 text_hover_shadow"></i>
-                </span>
-            </li>
+    const {taskItems,setTaskItems} = 
+    useContext(taskContext)
 
-            <li className="list-group-item d-flex justify-content-between list-group-item-success">
-                کار شماره 2
-                <span>
-                    <i className="me-3 pointer fas fa-check text-success transition_200 text_hover_shadow"></i>
-                    <i className="me-3 pointer fas fa-times text-warning transition_200 text_hover_shadow"></i>
-                    <i className="me-3 pointer fas fa-trash text-danger transition_200 text_hover_shadow"></i>
-                </span>
-            </li>
-
-        </ul>    
-    )
+    const handleSetDoneTask = (id)=>{
+        const index = taskItems.findIndex(t=>
+            t.id===id);
+        let newTaskItems = [...taskItems]
+        newTaskItems[index].done = !newTaskItems[index].done;
+        setTaskItems(newTaskItems);
+    }
+    const handleDeleteTask = (id)=>{
+        let newTasks = taskItems.filter(t=>
+            t.id != id);
+        setTaskItems(newTasks); 
+    }
+    if(taskItems.length){
+        return (
+            <ul className="list-group m-0 p-0 mt-2">
+                {
+                    taskItems.map(t=>(
+                        <li className=
+                        {`list-group-item d-flex 
+                        justify-content-between 
+                         ${t.done ? "list-group-item-success":
+                         ""}`}>
+                        {t.title}
+                        <span>
+                            {
+                                t.done ? (
+                                    <i className="me-3 pointer fas fa-times 
+                                    text-warning transition_200 
+                                    text_hover_shadow"
+                                    onClick=
+                                    {()=>{return handleSetDoneTask(t.id)}}></i>
+                                ) : (
+                                    <i className="me-3 pointer fas fa-check 
+                                    text-success transition_200 
+                                    text_hover_shadow" onClick=
+                                    {()=>{return handleSetDoneTask(t.id)}}></i>
+                                )
+                            }
+                            <i className="me-3 
+                            pointer fas fa-trash 
+                            text-danger transition_200 
+                            text_hover_shadow" onClick=
+                            {()=>handleDeleteTask(t.id)}></i>
+                        </span>
+                    </li>
+                    ))
+                }
+            </ul>    
+        )
+    }else{
+        return <h1 className='text-center text-danger mt-4'>هیچ کاری ثبت نشده...!</h1>
+    }
+   
 }
 
 export default TaskItems;
